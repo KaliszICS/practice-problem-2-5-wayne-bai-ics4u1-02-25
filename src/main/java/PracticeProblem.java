afb438da98635a58ddd449055b17af70e0e4c284
wayne-bai-ics4u1-02-25 <wayne.bai@student.tdsb.on.ca>
2025-03-25 02:55:54 +0000

    sefergerghrhtrh tr j ty jkty jyjgyjktyjk7u y jstr jty n,dtrj ntgr hstrnjo; hstrhilser e jmyio;er hio;h joper juip erjiop hetjz ip heruis trs trs trhs trhs trhs trh trhduip uips trhnil

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,41 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		insertionSort(new char[] {'b', 'f', 'a', 'c', 'g', 'd', 'e', '0', ' '});
 	}
 
+	// public static void print(char[] args) {
+	// 	for (int i = 0; i < args.length; i++) {
+	// 		System.out.print("\"" + args[i] + "\" ");
+	// 	}
+	// 	System.out.println();
+	// }
+
+	public static void insertionSort(char[] characters) {
+		
+		// print(characters);
+
+		for (int i = 0; i < characters.length; i++) {
+			
+			char swapThisValue = characters[i];
+			// don't compare yourself
+			int comparisonIndex = i - 1;
+
+			while (comparisonIndex >= 0 && swapThisValue < characters[comparisonIndex]) {
+
+				characters[comparisonIndex + 1] = characters[comparisonIndex];
+				
+				
+				comparisonIndex --;
+			}
+			characters[comparisonIndex + 1] = swapThisValue;
+			// print(characters);
+		}
+	}
 
 }
+
+/*
+  ## Questions
+1. Create a method called insertionSort(char[] characters) that accepts and array of characters and sorts it using the insertion sort algorithm. Do not return anything.
+*/
